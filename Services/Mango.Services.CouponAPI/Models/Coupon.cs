a136214850b8167b0d441a3722af5aef0d00f670
Brandon Zander <zan.bran@gmail.com>
2024-12-03 20:19:16 -0600

    Add coupon model and dto

---
+++ Services/Mango.Services.CouponAPI/Models/Coupon.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mango.Services.CouponAPI.Models;
+
+public class Coupon
+{
+    public int CouponId { get; set; }
+    public string CouponCode { get; set; }
+    public double DiscountAmount { get; set; }
+    public int MinAmount { get; set; }
+}
