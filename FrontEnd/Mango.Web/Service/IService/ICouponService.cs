94b0723813bda96235bd8b290ced6cd30b7fd295
Brandon Zander <zan.bran@gmail.com>
2024-12-09 13:36:39 -0600

    Add base coupon service skeleton

---
+++ FrontEnd/Mango.Web/Service/IService/ICouponService.cs
@@ -0,0 +1,13 @@
+using Mango.Web.Models;
+
+namespace Mango.Web.Service.IService;
+
+public interface ICouponService
+{
+    Task<ResponseDto?> GetCouponAsync(string couponCode);
+    Task<ResponseDto?> GetAllCouponsAsync();
+    Task<ResponseDto?> GetCouponByIdAsync(int id);
+    Task<ResponseDto?> CreateCouponAsync(CouponDto couponDto);
+    Task<ResponseDto?> UpdateCouponAsync(CouponDto couponDto);
+    Task<ResponseDto?> DeleteCouponAsync(int id);
+}
--- FrontEnd/Mango.Web/Utility/SD.cs
+++ FrontEnd/Mango.Web/Utility/SD.cs
@@ -4,6 +4,7 @@ namespace Mango.Web.Utility;
 
 public class SD
 {
+    public static string CouponAPIBase { get; set; }
     public enum ApiType
     {
         GET,
