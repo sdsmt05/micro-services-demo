53cddc2574aa2b33671c5a3b564bb468f17a3e04
Brandon Zander <zan.bran@gmail.com>
2024-12-11 14:35:10 -0600

    Add identity tables and db context

--- Services/Mango.Services.AuthAPI/Program.cs
+++ Services/Mango.Services.AuthAPI/Program.cs
@@ -1,7 +1,14 @@
+using Mango.Services.AuthAPI.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<AppDBContext>(option => {
+    option.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDBContext>().AddDefaultTokenProviders();
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -15,9 +22,19 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-
+ApplyMigration();
 app.Run();
+
+void ApplyMigration() {
+    using (var scope = app.Services.CreateScope()) {
+        var _db = scope.ServiceProvider.GetRequiredService<AppDBContext>();
+
+        if(_db.Database.GetPendingMigrations().Count() > 0) {
+            _db.Database.Migrate();
+        }  
+    }
+}
