94629e6d505f333d231b5d6b358761301b2a6510
Brandon Zander <zan.bran@gmail.com>
2024-12-16 17:22:19 -0600

    Add auth dtos

---
+++ Services/Mango.Services.AuthAPI/Models/Dto/LoginResponseDto.cs
@@ -0,0 +1,8 @@
+namespace Mango.Services.AuthAPI.Models.Dto
+{
+    public class LoginResponseDto
+    {
+        public UserDto User { get; set; }
+        public string Token { get; set; }
+    }
+}
