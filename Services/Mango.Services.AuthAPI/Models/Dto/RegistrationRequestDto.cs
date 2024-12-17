c74675def1053a6cca8d8abca2e618abdfc650ab
Brandon Zander <zan.bran@gmail.com>
2024-12-17 12:54:17 -0600

    Add role functionality to auth service and controller

--- Services/Mango.Services.AuthAPI/Models/Dto/RegistrationRequestDto.cs
+++ Services/Mango.Services.AuthAPI/Models/Dto/RegistrationRequestDto.cs
@@ -6,5 +6,6 @@ namespace Mango.Services.AuthAPI.Models.Dto
         public string Name { get; set; }
         public string PhoneNumber { get; set; }
         public string Password { get; set; }
+        public string? Role { get; set; }
     }
 }
--- Services/Mango.Services.AuthAPI/Services/AuthService.cs
+++ Services/Mango.Services.AuthAPI/Services/AuthService.cs
@@ -21,6 +21,22 @@ namespace Mango.Services.AuthAPI.Services
             _jwtTokenGenerator = jwtTokenGenerator;
         }
 
+        public async Task<bool> AssignRole(string email, string roleName)
+        {
+            var user = _db.ApplicationUsers.FirstOrDefault(u => u.Email.ToLower() == email.ToLower());
+            if (user != null)
+            {
+                if(!_roleManager.RoleExistsAsync(roleName).GetAwaiter().GetResult())
+                {
+                    //Create role if it does not exist
+                    _roleManager.CreateAsync(new IdentityRole(roleName)).GetAwaiter().GetResult();
+                }
+                await _userManager.AddToRoleAsync(user, roleName);
+                return true;
+            }
+            return false;
+        }
+
         public async Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
         {
             var user = _db.ApplicationUsers.FirstOrDefault(u => u.UserName.ToLower() == loginRequestDto.UserName.ToLower());
