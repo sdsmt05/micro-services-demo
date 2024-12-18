daac3caec244cdab43fc5d1421d5c63843a01236
Brandon Zander <zan.bran@gmail.com>
2024-12-18 13:35:58 -0600

    Add role dropdown to register view

--- FrontEnd/Mango.Web/Controllers/AuthController.cs
+++ FrontEnd/Mango.Web/Controllers/AuthController.cs
@@ -1,6 +1,8 @@
 using Mango.Web.Models;
 using Mango.Web.Service.IService;
+using Mango.Web.Utility;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Mango.Web.Controllers
 {
@@ -22,6 +24,13 @@ namespace Mango.Web.Controllers
         [HttpGet]
         public IActionResult Register()
         {
+            var roleList = new List<SelectListItem>()
+            {
+                new SelectListItem{Text=SD.RoleAdmin, Value=SD.RoleAdmin},
+                new SelectListItem{Text=SD.RoleCustomer, Value=SD.RoleCustomer}
+            };
+
+            ViewBag.RoleList = roleList;
             return View();
         }
 
