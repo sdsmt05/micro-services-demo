f8639cf75a87d51838e84753428bdae7de72b443
Brandon Zander <zan.bran@gmail.com>
2024-12-18 18:26:55 -0600

    Add login functionality

--- FrontEnd/Mango.Web/Controllers/AuthController.cs
+++ FrontEnd/Mango.Web/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using Mango.Web.Service.IService;
 using Mango.Web.Utility;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
 
 namespace Mango.Web.Controllers
 {
@@ -19,6 +20,23 @@ namespace Mango.Web.Controllers
         {
             LoginRequestDto loginRequestDto = new();
             return View(loginRequestDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginRequestDto obj)
+        {
+            ResponseDto responseDto = await _authService.LoginAsync(obj);
+
+            if (responseDto != null && responseDto.IsSuccess)
+            {
+                LoginResponseDto loginResponseDto = JsonConvert.DeserializeObject<LoginResponseDto>(Convert.ToString(responseDto.Result));
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("CustomError", responseDto.Message);
+                return View(obj);
+            }
         }
 
         [HttpGet]
@@ -40,14 +58,14 @@ namespace Mango.Web.Controllers
             ResponseDto result = await _authService.RegisterAsync(obj);
             ResponseDto assignRole;
 
-            if(result != null && result.IsSuccess)
+            if (result != null && result.IsSuccess)
             {
-                if(string.IsNullOrEmpty(obj.Role))
+                if (string.IsNullOrEmpty(obj.Role))
                 {
                     obj.Role = SD.RoleCustomer;
                 }
                 assignRole = await _authService.AssignRoleAsync(obj);
-                if(assignRole != null & assignRole.IsSuccess)
+                if (assignRole != null & assignRole.IsSuccess)
                 {
                     TempData["success"] = "Registration Successful";
                     return RedirectToAction(nameof(Login));
