444518e6282363aa6d03695b7bb35cf6fc9e7d21
Brandon Zander <zan.bran@gmail.com>
2024-12-19 12:40:33 -0600

    Update login and register validations

--- FrontEnd/Mango.Web/Controllers/AuthController.cs
+++ FrontEnd/Mango.Web/Controllers/AuthController.cs
@@ -42,7 +42,7 @@ namespace Mango.Web.Controllers
             }
             else
             {
-                ModelState.AddModelError("CustomError", responseDto.Message);
+                TempData["error"] = responseDto.Message;
                 return View(obj);
             }
         }
@@ -78,6 +78,10 @@ namespace Mango.Web.Controllers
                     TempData["success"] = "Registration Successful";
                     return RedirectToAction(nameof(Login));
                 }
+            }
+            else
+            {
+                TempData["error"] = result.Message;
             }
 
             var roleList = new List<SelectListItem>()
