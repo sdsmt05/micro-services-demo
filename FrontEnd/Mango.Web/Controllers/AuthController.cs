be8529827d1068f6502a4f51dd529509ce64226b
Brandon Zander <zan.bran@gmail.com>
2024-12-18 18:14:53 -0600

    Add register functionality

--- FrontEnd/Mango.Web/Controllers/AuthController.cs
+++ FrontEnd/Mango.Web/Controllers/AuthController.cs
@@ -34,6 +34,36 @@ namespace Mango.Web.Controllers
             return View();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Register(RegistrationRequestDto obj)
+        {
+            ResponseDto result = await _authService.RegisterAsync(obj);
+            ResponseDto assignRole;
+
+            if(result != null && result.IsSuccess)
+            {
+                if(string.IsNullOrEmpty(obj.Role))
+                {
+                    obj.Role = SD.RoleCustomer;
+                }
+                assignRole = await _authService.AssignRoleAsync(obj);
+                if(assignRole != null & assignRole.IsSuccess)
+                {
+                    TempData["success"] = "Registration Successful";
+                    return RedirectToAction(nameof(Login));
+                }
+            }
+
+            var roleList = new List<SelectListItem>()
+            {
+                new SelectListItem{Text=SD.RoleAdmin, Value=SD.RoleAdmin},
+                new SelectListItem{Text=SD.RoleCustomer, Value=SD.RoleCustomer}
+            };
+
+            ViewBag.RoleList = roleList;
+            return View(obj);
+        }
+
         public IActionResult Logout()
         {
             return View();
